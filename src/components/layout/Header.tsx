8b6ca76a95ae95518174172fd3e3253f935dde12
태양 <zxcv2987@naver.com>
2025-05-12 00:26:15 +0900

    fix: font 추가 및 layout 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="sticky top-0 z-10 p-4 px-8 bg-white/70 backdrop-blur-md border-b border-zinc-200/50">
+    <header className="sticky top-0 z-10 p-4 md:px-16 bg-white/60 backdrop-blur-md border-b border-zinc-200/50">
       <div className="flex justify-between items-center">
-        <div className="flex items-end justify-center gap-2 h-fit">
+        <Link href="/" className="flex items-end justify-center h-fit">
           <h1 className="text-3xl font-bold">dere</h1>
+          <span className="text-xs text-zinc-500 p-1 pl-2">|</span>
           <span className="text-xs text-zinc-500 p-1">DevRecord</span>
-        </div>
+        </Link>
       </div>
     </header>
   );
