e495c86bcca09d7abaec0ae74cfb078fa2e3b190
태양 <zxcv2987@naver.com>
2025-05-12 15:34:06 +0900

    feat: 코드 하이라이팅 적용 및 rehype, remark 패키지 추가 - remark gfm - rehype title,slug,autoLinkHeadings - prettyCode, shiki

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,12 +1,25 @@
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { getAllFrontMatter, getPostBySlug } from "@/utils/post";
 import { format } from "date-fns";
+import rehypePrettyCode from "rehype-pretty-code";
+import remarkGfm from "remark-gfm";
+import rehypeCodeTitles from "rehype-code-titles";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 export async function generateStaticParams() {
   return getAllFrontMatter().map((post) => ({
     slug: post.slug,
   }));
 }
+
+/** @type {import('rehype-pretty-code').Options} */
+const prettyCodeOptions = {
+  theme: {
+    dark: "one-dark-pro",
+    light: "github-light",
+  },
+};
 
 export default async function Page({
   params,
@@ -17,8 +30,8 @@ export default async function Page({
   const { content, data } = getPostBySlug(slug);
 
   return (
-    <article className="w-full flex flex-col gap-8">
-      <section className="w-full flex flex-col gap-2">
+    <article className="w-full flex flex-col gap-12">
+      <section className="w-full flex flex-col gap-3 border-b border-zinc-200 pb-12">
         <span className="text-zinc-500 text-sm">
           {format(new Date(data.date), "yyyy년 MM월 dd일")}
         </span>
@@ -27,7 +40,24 @@ export default async function Page({
       </section>
       <section className="w-full overflow-hidden">
         <div className="prose-container w-full max-w-full overflow-x-auto py-4">
-          <MDXRemote source={content} />
+          <MDXRemote
+            source={content}
+            options={{
+              mdxOptions: {
+                remarkPlugins: [remarkGfm],
+                rehypePlugins: [
+                  rehypeCodeTitles,
+                  rehypeSlug,
+                  [
+                    rehypeAutolinkHeadings,
+                    { properties: { className: ["anchor"] } },
+                  ],
+                  [rehypePrettyCode, prettyCodeOptions],
+                ],
+                format: "mdx",
+              },
+            }}
+          />
         </div>
       </section>
     </article>
